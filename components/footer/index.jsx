a1734fdec85fe8ab5703d3fd094ec7af20470388
La Diega <83097524+Ladiega@users.noreply.github.com>
2023-05-01 22:13:24 -0500

    adding instagram logo

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,15 +1,28 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import instaLogo from '../../public/img/instagramLogo.svg'
+
 export default function Footer(){
   return(
     <div>
       <footer className='footer_global_container  text-white'>
         <div className="footer_container flex flex-col">
-          footer
-          <div className="fotter_text ml-5">©2023 OnlyFlashTattoo 
+          
+          <div className="fotter_text ml-5 mt-5 mb-5">©2023 OnlyFlashTattoo 
             <div className="footer_instagram_container">
-              <div className="instagram_logo">instagram logo</div>
+              <div className="instagram_logo mb-5">
+                <Link href='https://www.instagram.com/onlyflashtattoo/'>
+                  <Image
+                  src={instaLogo}
+                  alt="instagram"
+                  width={30}
+                  className='instagramLogo rounded-md'
+                  />
+              </Link>
+              </div>
 
             </div>
-            </div>          
+          </div>          
         </div>
         
       </footer>
