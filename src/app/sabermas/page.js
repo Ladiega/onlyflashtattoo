eff2637ad76693d2cd68b8f2d3715ff699f84cdf
La Diega <83097524+Ladiega@users.noreply.github.com>
2023-05-01 23:28:22 -0500

    sabermas text

--- src/app/sabermas/page.js
+++ src/app/sabermas/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
+import Logo from '../../../public/img/logo3.png'
 import Link from "next/link"
-import CataProfile from '../../../public/img/cataProfile.jpeg'
+import Crew from '../../../public/onlyflashcrew.png'
 
 
 
@@ -20,7 +21,7 @@ export default function SaberMas(){
     '@helmuthtattoo',
     '@ladiegatattoo']
   const instagramUrl =[
-    '/',
+    'https://www.instagram.com/onlyflashtattoo',
     'https://www.instagram.com/malandrinatattoo',
     'https://www.instagram.com/nevatattoos',
     'https://www.instagram.com/pinzoncarlos777',
@@ -36,19 +37,24 @@ export default function SaberMas(){
     <div className="card_container flex bg-white mr-5 ml-5 mt-10 mb-5 rounded-lg ">
       <div className='image-container ml-5 mr-1 mt-10'>
         <Image
-        src={CataProfile}
+        src={Logo}
         alt='logo'
-        width={150}
+        width={250}
         className="rounded-full"
         />
       </div>
       <div className='card_info_container mt-10'>
-        <div className="name font-bold"><h1>{nombre[1]}</h1></div>
-        <div className="instagram font-light"><Link className=' text-gray-500' href={instagramUrl[1]}>{instagram[1]}</Link></div>
+        <div className="name font-bold"><h1>{nombre[0]}</h1></div>
+        <div className="instagram font-light"><Link className=' text-gray-500' href={instagramUrl[0]}>{instagram[0]}</Link></div>
         <div className="text mr-10 mt-5">
-          <p> Semana a semana un gurpo selecto de tatuadores les tendrá un pack de diseños únicos para tatuar, en una sexi hoja de flash, a tentadores precios.
+          <p> Muchas veces pensamos en como podemos impactar nuestro entorno de una forma distinta a los que estamos siendo encaminados, sin tener una noción muy clara de este proceso inmediato.  Las redes sociales son un entorno en el cual pasamos mucho tiempo y logran influenciar muchas de nuestras decisiones. 
 
-        También podrás adquirir una membresía con la que tendrás descuentos. preoridad en citas, y muchas mas para nuestro clientes vip.</p>
+Es por esto que nace nuestra necesidad de crear OnlyFlashTattoo y usar estos medios para llegar a todos los amantes del tatuaje o ofrecer muchas ideas para ser tatuadas. <br/><br/>
+Onlyflash esta integrado por: <b className="names_intro font-black">Nelson Neva, Carlos Pinzón, Catalina Fonseca, Helmuth Sanabria y Diego Gutierrez . </b>
+
+Semana a semana tendremos un pack de diseños únicos para tatuar, en una sexi hoja de flash, a tentadores precios. También podrás adquirir una membresía con la que tendrás descuentos. prioridad en citas, y muchas mas para nuestro clientes vip.
+
+Muchas gracias por leernos, apoyarnos, preguntarnos y tatuarse… Recuerda que Tattoo es para siempre   !!!.</p>
 
 
 
@@ -56,13 +62,13 @@ export default function SaberMas(){
 </div>
         <div className="photo mr-10 mt-10">
           <Image
-          src={CataProfile}
+          src={Crew}
           alt={instagram[1]}
           className=" rounded-md"
           
         />
         </div>
-        <div className="more mt-10 mb-10"><Link href='/sabermas'>saber mas...</Link> </div>
+        <div className="more mt-10 mb-10"><Link href='/'>saber mas sobre cada uno...</Link> </div>
       </div>
     </div>
     </section>
