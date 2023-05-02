4a9ec13b83e8495c8f71c8fa2a69c360989fc96b
ladiega <fokuiatodo@gmail.com>
2023-05-02 18:36:45 -0500

    footer bg changes

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -5,10 +5,13 @@ import instaLogo from '../../public/img/instagramLogo.svg'
 export default function Footer(){
   return(
     <div>
-      <footer className='footer_global_container  text-white'>
+      <footer className='footer_global_container bg-white text-black'>
         <div className="footer_container flex flex-col">
           
-          <div className="fotter_text ml-5 mt-5 mb-5">©2023 OnlyFlashTattoo 
+          <div className="fotter_text ml-5 mt-5 mb-5 ">
+            
+            <div className='text-xs'>©2023 OnlyFlashTattoo </div> 
+            <div className='text-xs'>Contacto</div>
             <div className="footer_instagram_container">
               <div className="instagram_logo mb-5">
                 <Link href='https://www.instagram.com/onlyflashtattoo/'>
@@ -19,6 +22,8 @@ export default function Footer(){
                   className='instagramLogo rounded-md'
                   />
               </Link>
+              <span className='foote_line w-1 h-1'></span>
+              
               </div>
 
             </div>
